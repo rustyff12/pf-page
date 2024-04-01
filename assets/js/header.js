5a4592b85fa5c730a6666c1eae6bd49dd5167c6c
rustyff12 <rustyff123@gmail.com>
2024-04-01 03:00:13 +0200

    adjust mobile nav

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,12 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
-
+const aHover = document.querySelectorAll(".navbar li");
 hamburger.addEventListener("click", () => {
-    console.log("click event");
     const navBar = document.querySelector(".navbar-mobile");
     if (!navBar.classList.contains("show")) {
-        console.log("inside");
         navBar.classList.add("show");
     } else {
         navBar.classList.remove("show");
     }
 });
+
+/* aHover.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+        console.log(e.target);
+        // item.classList.add("li-hover");
+    });
+}); */
